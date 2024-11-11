b24ecc81ad68612b98ed1f31090c76a13f8b0da9
libc0607 <libc0607@gmail.com>
2024-11-11 20:48:11 +0800

    Fix build error

--- os_dep/linux/rtw_proc.c
+++ os_dep/linux/rtw_proc.c
@@ -6134,6 +6134,7 @@ static ssize_t proc_set_single_tone(struct file *file, const char __user *buffer
 	return count;
 }
 
+#ifdef CONFIG_BEAMFORMING_MONITOR
 static int proc_get_bf_monitor_conf(struct seq_file *m, void *v)
 {
 	struct net_device *dev = m->private;
@@ -6300,7 +6301,7 @@ static ssize_t proc_set_bf_monitor_en(struct file *file, const char __user *buff
         RTW_INFO("bf_monitor_enable, %hhu \n", en);
 	return count;
 }
-
+#endif
 
 /*
 * rtw_adapter_proc:
